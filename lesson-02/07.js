bbe45e53cd72f5a552ce448d29ca55032c6e2235
Sokolov Sergei <serejasokolov@gmail.com>
2023-11-16 00:54:41 +0300

    test 4,5 - 2

---
+++ lesson-02/07.js
@@ -0,0 +1,13 @@
+// code
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+console.log(passport.name);
+    passport = {
+    name: "Ivan",
+    surname: "Petrov",
+};
+console.log(passport.name);
+
+
