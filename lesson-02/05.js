96ee086f5a44cfb1923b8d17e0e3a32176d12e3d
Sokolov Sergei <serejasokolov@gmail.com>
2023-11-20 06:54:46 +0300

    test 4 -2

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,11 +1,1 @@
 // code
-let passport = {
-    name: "Petr",
-    surname: "Petrov",
-};
-let copyPassport = {...passport} 
-
-copyPassport.name = "Ivan";
-
-//console.log(passport.name);
-//console.log(copyPassport.name);
