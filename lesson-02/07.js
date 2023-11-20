bc9120ae86a0aced50e3e917795111e6bed65d47
Sokolov Sergei <serejasokolov@gmail.com>
2023-11-20 17:13:07 +0300

    test 7- 2

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,1 +1,14 @@
 // code
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let copyPassportMarried = {...passportMarried,married: true, address: { ...passportMarried.address}}
+
+console.log(passportMarried);
+console.log(copyPassportMarried);
