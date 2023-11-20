948e60d43fe4f481f6fe298e86c16c1e111bb877
Sokolov Sergei <serejasokolov@gmail.com>
2023-11-20 07:30:36 +0300

    test 6 - 2

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,1 +1,16 @@
 // code
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let copyPassportWithAddress = {...passportWithAddress};
+
+let oldCity = passportWithAddress.address.city;
+let newCity = copyPassportWithAddress.address.city="Bobryisk";
+console.log(oldCity);
+console.log(newCity);
